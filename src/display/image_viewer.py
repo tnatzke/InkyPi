5c21e7ed2e5ace0d992485a84f634a4396fd9751
tnatzke <29801322+tnatzke@users.noreply.github.com>
2025-07-28 18:17:30 -0500

    Add support for regular monitors.

---
+++ src/display/image_viewer.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import os
+import hashlib
+import time
+import argparse
+
+class ImageViewerApp:
+    def __init__(self, master, image_path, timeout):
+        self.master = master
+
+        self.master.attributes('-fullscreen', True)  # Set window to fullscreen
+        self.master.bind('<Escape>', lambda e: self.master.destroy())  # Exit on Escape key
+
+        self.timeout = timeout
+        self.image_path = image_path
+        self.current_hash = self.get_file_hash()
+
+        self.label = tk.Label(master)
+        self.label.pack()
+
+        self.width, self.height = self.master.winfo_screenwidth(), self.master.winfo_screenheight()
+
+        self.load_image()
+        self.check_for_changes()
+
+    def get_file_hash(self):
+        if not os.path.exists(self.image_path):
+            return None
+        with open(self.image_path, 'rb') as f:
+            return hashlib.md5(f.read()).hexdigest()
+
+    def load_image(self):
+        try:
+            img = Image.open(self.image_path)
+            img = img.resize((self.width, self.height), Image.LANCZOS)
+            self.tk_image = ImageTk.PhotoImage(img)
+            self.label.config(image=self.tk_image)
+        except FileNotFoundError:
+            self.label.config(text="Image not found!")
+            self.tk_image = None
+        except Exception as e:
+            self.label.config(text=f"Error loading image: {e}")
+            self.tk_image = None
+
+    def check_for_changes(self):
+        new_hash = self.get_file_hash()
+        if new_hash != self.current_hash:
+            self.current_hash = new_hash
+            self.load_image()
+        self.master.after(self.timeout, self.check_for_changes) # Check every 1 second
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='A simple program that displays image file changes.')
+
+    parser.add_argument('-f', '--filename', type=str, required=True, help='Filename of a image to monitor.') # Optional argument with a value
+    parser.add_argument('-t', '--timeout', type=int, required=True, help='How often the file should be checked for changes.') # Flag (boolean)
+
+    args = parser.parse_args()
+
+    root = tk.Tk()
+    app = ImageViewerApp(root, args.filename, args.timeout)
+    root.mainloop()
---
+++ src/display/monitor_display.py
@@ -0,0 +1,94 @@
+import logging
+import os
+import tkinter as tk
+import tempfile
+import subprocess
+import atexit
+import sys
+from pathlib import Path
+
+from PIL import Image, ImageTk
+from display.abstract_display import AbstractDisplay
+
+logger = logging.getLogger(__name__)
+
+
+class MonitorDisplay(AbstractDisplay):
+    root = None
+    screen_width: int = None
+    screen_height: int = None
+    label = None
+
+    """
+    Handles the Inky e-paper display.
+
+    This class initializes and manages interactions with the Inky display,
+    ensuring proper image rendering and configuration storage.
+
+    The Inky display driver supports auto configuration.
+    """
+
+    def initialize_display(self):
+        """
+        Initializes the Inky display device.
+
+        Sets the display border and stores the display resolution in the device configuration.
+
+        Raises:
+            ValueError: If the resolution cannot be retrieved or stored.
+        """
+
+        root = tk.Tk()
+        root.attributes('-fullscreen', True)
+        self.screen_width = root.winfo_screenwidth()
+        self.screen_height = root.winfo_screenheight()
+
+        self.tmp_file = tempfile.NamedTemporaryFile(suffix=".png").name
+
+        # Define the path to the subprocess script and its arguments
+        subprocess_script = ["python", Path(__file__).parent.joinpath("image_viewer.py").resolve(), "-f", self.tmp_file,
+                             "-t", "3000"]
+
+        # Start the subprocess
+        self.viewer = subprocess.Popen(subprocess_script)
+        atexit.register(self.cleanup_display)
+
+        # store display resolution in device config
+        # if not self.device_config.get_config("resolution"):
+        #     self.device_config.update_value(
+        #         "resolution",
+        #         [int(self.screen_width), int(self.screen_height)],
+        #         write=True)
+
+    def display_image(self, image, image_settings=[]):
+        """
+        Displays the provided image on the Inky display.
+
+        The image has been processed by adjusting orientation and resizing 
+        before being sent to the display.
+
+        Args:
+            image (PIL.Image): The image to be displayed.
+            image_settings (list, optional): Additional settings to modify image rendering.
+
+        Raises:
+            ValueError: If no image is provided.
+        """
+        with open(self.tmp_file, mode="w+b") as temp_file:
+            image.save(temp_file, format="PNG")
+
+    def terminate_subprocess(self):
+        if self.viewer.poll() is None:  # Check if the subprocess is still running
+            print(f"Terminating subprocess (PID: {self.viewer.pid})...")
+            self.viewer.terminate()
+            self.viewer.wait(timeout=5)  # Wait for a short period for graceful termination
+            if self.viewer.poll() is None:
+                print("Subprocess did not terminate gracefully, killing...")
+                self.viewer.kill()
+        else:
+            print("Subprocess already terminated.")
+
+    def cleanup_display(self):
+        self.terminate_subprocess()
+        if os.path.exists(self.tmp_file):
+            os.remove(self.tmp_file)
