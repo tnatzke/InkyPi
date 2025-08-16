d0889fef60fbbb963dcef452fbe3691796ac50e4
zapper1942 <160916879+zapper1942@users.noreply.github.com>
2025-08-16 08:08:54 -0400

    Adding a randamization option to the Photo Upload Plugin (#243)
    * Adding a randamization option to the Photo Upload Plugin
    
    * change execution order so when randomization is on the first picture is also random. Update the Randomization label to be more clear what it does.
    
    * revert inprogress html changes
    
    * revert debug pathing changes
    
    * fix index out of range issue

--- src/plugins/image_upload/image_upload.py
+++ src/plugins/image_upload/image_upload.py
@@ -2,18 +2,13 @@ from plugins.base_plugin.base_plugin import BasePlugin
 from PIL import Image, ImageOps, ImageColor
 from io import BytesIO
 import logging
+import random
 
 logger = logging.getLogger(__name__)
 
+
 class ImageUpload(BasePlugin):
-    def generate_image(self, settings, device_config):
-        img_index = settings.get("image_index", 0)
-        image_locations = settings.get("imageFiles[]")
-
-        if img_index >= len(image_locations):
-            # reset if image_locations changed
-            img_index = 0
-
+    def open_image(self, img_index: int, image_locations: list) -> Image:
         if not image_locations:
             raise RuntimeError("No images provided.")
         # Open the image using Pillow
@@ -22,8 +17,29 @@ class ImageUpload(BasePlugin):
         except Exception as e:
             logger.error(f"Failed to read image file: {str(e)}")
             raise RuntimeError("Failed to read image file.")
+        return image
+        
 
-        settings['image_index'] = (img_index + 1) % len(image_locations)
+    def generate_image(self, settings, device_config) -> Image:
+        
+        # Get the current index from the device json
+        img_index = settings.get("image_index", 0)
+        image_locations = settings.get("imageFiles[]")
+
+        if img_index >= len(image_locations):
+            # Prevent Index out of range issues when file list has changed
+            img_index = 0
+
+        if settings.get('randomize') == "true":
+            img_index = random.randrange(0, len(image_locations))
+            image = self.open_image(img_index, image_locations)
+        else:
+            image = self.open_image(img_index, image_locations)
+            img_index = (img_index + 1) % len(image_locations)
+
+        # Write the new index back ot the device json
+        settings['image_index'] = img_index
+
         ###
         if settings.get('padImage') == "true":
             dimensions = device_config.get_resolution()
