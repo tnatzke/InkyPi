09986b8c2f406faf92015e2fc34d0fb09091817a
Fatih Ak <fatihak@berkeley.edu>
2025-11-18 21:09:41 -0500

    Fix charset when loading js, remove fullcalendar css

--- install/update_vendors.sh
+++ install/update_vendors.sh
@@ -12,9 +12,6 @@ curl -L https://code.jquery.com/jquery-3.6.0.min.js -o src/static/scripts/jquery
 echo "Updating Chart JS"
 curl -L https://cdn.jsdelivr.net/npm/chart.js -o src/static/scripts/chart.js
 
-echo "Updating fullcalendar CSS"
-curl -L https://cdn.jsdelivr.net/npm/fullcalendar@6.1.17/index.global.min.css -o src/static/styles/calendar.min.css
-
 echo "Updating fullcalendar JS"
 curl -L https://cdn.jsdelivr.net/npm/fullcalendar@6.1.17/index.global.min.js -o src/static/scripts/calendar.min.js
 
