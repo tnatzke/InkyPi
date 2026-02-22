3a8faf58c62ef6f7131f1a06cd9f74d67c677bd8
Tim Natzke <29801322+tnatzke@users.noreply.github.com>
2026-02-22 12:23:40 -0600

    Add test image path.

--- tests/test-screen.py
+++ tests/test-screen.py
@@ -15,7 +15,7 @@ size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
 screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 
 # Load and display the image
-image = pygame.image.load("your_image.jpg")
+image = pygame.image.load("/home/tmn/projects/InkyPi/docs/images/ink_clock.png")
 image = pygame.transform.scale(image, size) # Scale to fit screen
 screen.blit(image, (0, 0))
 pygame.display.update()
