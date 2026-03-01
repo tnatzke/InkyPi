11e05ddff0342cc34df18198666c57c8c46c472f
Tim Natzke <29801322+tnatzke@users.noreply.github.com>
2026-03-01 13:16:38 -0600

    Fix variable name.

--- src/display/monitor_display.py
+++ src/display/monitor_display.py
@@ -85,7 +85,7 @@ class MonitorDisplay(AbstractDisplay):
             image.save(temp_file, format="PNG")
             logger.info("Image saved to {}".format(self.tmp_file))
             pygame_image = pygame.image.load(temp_file)
-            pygame_image = pygame.transform.scale(image, self.size)  # Scale to fit screen
+            pygame_image = pygame.transform.scale(pygame_image, self.size)  # Scale to fit screen
             self.screen.blit(pygame_image, (0, 0))
             pygame.display.update()
             logger.info("Image displayed")
