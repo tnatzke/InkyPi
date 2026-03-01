cb6cafc637c713ad795abbbd79d29d5fa8b7516b
Tim Natzke <29801322+tnatzke@users.noreply.github.com>
2026-03-01 12:07:59 -0600

    Cleanup test for monitor displays.

--- tests/test_monitor_display.py
+++ tests/test_monitor_display.py
@@ -1,18 +1,15 @@
 import unittest
 from time import sleep
-
 from PIL import Image
-
-from display.monitor_display import MonitorDisplay
-
+from src.display.monitor_display import MonitorDisplay
 
 class TestMonitorDisplay(unittest.TestCase):
     def test_displaying_images(self):
         display = MonitorDisplay(None)
 
-        images = [r"C:\Users\Tim Natzke\PycharmProjects\InkyPi\docs\images\inky_clock.jpg",
-                  r"C:\Users\Tim Natzke\PycharmProjects\InkyPi\docs\images\raspberry_pi_imager.png",
-                  r"C:\Users\Tim Natzke\PycharmProjects\InkyPi\docs\images\raspberry_pi_imager_general.png"]
+        images = ["/home/tmn/projects/InkyPi/docs/images/inky_clock.jpg",
+                  "/home/tmn/projects/InkyPi/docs/images/raspberry_pi_imager.png",
+                  "/home/tmn/projects/InkyPi/docs/images/raspberry_pi_imager_general.png"]
 
         for file in images:
             img = Image.open(file)
@@ -20,7 +17,6 @@ class TestMonitorDisplay(unittest.TestCase):
             sleep(10)
 
         display.cleanup_display()
-
 
 if __name__ == '__main__':
     unittest.main()
